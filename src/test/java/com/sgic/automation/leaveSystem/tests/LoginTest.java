0fe4a0595936d01ffbf812e96501fcc92f6aed4a
unknown <yokarasakumaran7@gmail.com>
2021-03-26 15:56:10 +0530

    addform

--- src/test/java/com/sgic/automation/leaveSystem/tests/LoginTest.java
+++ src/test/java/com/sgic/automation/leaveSystem/tests/LoginTest.java
@@ -1,6 +1,8 @@
 package com.sgic.automation.leaveSystem.tests;
 
+import com.sgic.automation.leaveSystem.pages.hrm.AdminPage;
 import com.sgic.automation.leaveSystem.pages.hrm.LoginPage;
+import com.sgic.automation.leaveSystem.pages.hrm.RequirmentPage;
 import com.sgic.automation.leaveSystem.testData.TestData;
 import com.sgic.automation.leaveSystem.utils.TestBase;
 import org.testng.annotations.Test;
@@ -12,11 +14,14 @@ public class LoginTest extends TestBase {
         softAssert = new SoftAssert();
         softAssert.assertTrue(LoginPage.isLoginPageDisplay(), "Login Page is not Display");
         softAssert.assertTrue(LoginPage.isUserNameDisplay(), "Login username is not Display");
-        LoginPage.setUserName(username);
+        LoginPage.setUserName("Admin");
         softAssert.assertTrue(LoginPage.isPasswordDisplay(), "Login Password is not Display");
-        LoginPage.setPassword(pass);
+        LoginPage.setPassword("admin123");
         softAssert.assertTrue(LoginPage.isLoginbuttonDisplay(), "Login button is not Display");
         LoginPage.clickLogin();
+
+
+
 
         softAssert.assertAll();
 
